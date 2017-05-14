2c95dde4b03c1a0cb7bf83519387df8551a4bdfa
Aizaz Khaja <aizazkj@gmail.com>
2017-05-14 08:34:05 -0400

    progress commit. added and tested model for wiki. associated user to wiki as well.

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:wiki) { create(:wiki, user: user) }
+  let(:user) { create(:user) }
+
+  it { is_expected.to belong_to(:user) }
+
+  it { is_expected.to validate_presence_of(:user) }
+
+  describe "attributes" do
+     it "has title, body and user attributes" do
+       expect(wiki).to have_attributes(title: wiki.title, body: wiki.body, user: user)
+     end
+  end
 end
